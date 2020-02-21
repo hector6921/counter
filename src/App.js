df87cfd5d4452f933bee89c04c5d7decb8918264
unknown <hector_6921@hotmail.com>
2020-02-20 20:13:13 -0800

    added the increment and decrement methods yay :)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import React, {Component} from 'react';
 
 class Counter extends Component{
+  constructor(){
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+// this is where all the methods go
+increment = () => {
+  this.setState({
+
+    count: this.state.count + 1,
+  })
+}
+decrement = () => {
+  this.setState({
+    count: this.state.count - 1,
+  })
+}
+clear = () => {
+  this.setState({
+    count: 0,
+  })
+}
+doubleCount = () => {
+  this.setState({
+    count: this.state.count + 2,
+  })
+}
+
   render(){
     return(
-      <div></div>
+      <div className = "container">
+      <div className = "navbar">Counter</div>
+      <div className = "counter">
+        <h1>{this.state.count}</h1>
+          <button type= "button" onClick={this.clear}>Clear</button>
+        <button type="button" onClick={this.increment}>Increment</button>
+        <button type= "button" onClick={this.decrement}>Decrement</button>
+        <button type= "button" onClick={this.doubleCount}>Double Count</button>
+      </div>
+      </div>
     )
   }
 }
